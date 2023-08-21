af19975c808da45a1e332df8167967153ea45a0e
David A. Imel <donalgrant@gmail.com>
2023-08-21 12:06:12 -0700

    Updated cross shape, converted to Wheel object everywhere

--- spiro.py
+++ spiro.py
@@ -66,15 +66,22 @@ class SpiroFig:
 
     def save_fig(self,filename='spiro.png'):  plt.savefig(filename,bbox_inches='tight')
 
-def spiro(R=10,a=4.0,b=3.5,loops=5,
+class Wheel:   # might consider making "spacing" part of the Wheel's data
+
+    def __init__(self,radius=3.0, pen=2.0, offset=0):
+        self.r = radius # radius of the wheel
+        self.m = pen    # marker position
+        self.o = offset # cw angle from the vertical
+
+def spiro(R=10,wheel=Wheel(4,3.5,0.0),loops=5,
           slide = lambda t: 1,
-          offset=0,spacing=pi/4000,orient=0):
-    return spiro_arc(x0=0,y0=0,orient=orient,R=R,a=a,b=b,loops=loops,
-                     slide=slide,offset=offset,
+          spacing=pi/4000,orient=0):
+    return spiro_arc(x0=0,y0=0,orient=orient,R=R,wheel=wheel,loops=loops,
+                     slide=slide,
                      spacing=spacing,invert=True,reverse=False)
 
-def spiro_arc(x0=0,y0=0,orient=0,R=10.0,a=4.0,b=3.5,
-              loops=1,offset=0,spacing=pi/4000,
+def spiro_arc(x0=0,y0=0,orient=0,R=10.0,wheel=Wheel(4,3.5,0),
+              loops=1,spacing=pi/4000,
               slide = lambda t: 1,
               start_guard=0,end_guard=0,start_guard_angle=0,end_guard_angle=0,
               invert=False,reverse=False):  
@@ -86,6 +93,10 @@ def spiro_arc(x0=0,y0=0,orient=0,R=10.0,a=4.0,b=3.5,
     iv = -1 if invert else 1
     sd = SpiroData()
 
+    a = wheel.r
+    b = wheel.m
+    offset = wheel.o
+    
     # work out guard effects:  change in arc-length
     if start_guard_angle==0:  start_guard_angle = start_guard/(R+iv*a)
     if end_guard_angle==0:    end_guard_angle   = end_guard/(R+iv*a)
@@ -125,9 +136,11 @@ def spiro_arc(x0=0,y0=0,orient=0,R=10.0,a=4.0,b=3.5,
     
     return sd
 
-def spiro_steps(R=10,a=4,b=3.5,loops=1,n=10,offset=pi/10,spacing=pi/2000):
+def spiro_steps(R=10,wheel=Wheel(4,3.5,0),loops=1,n=10,offset=pi/10,spacing=pi/2000):
     sd = SpiroData()
-    for i in range(n): sd.add(spiro(R=R,a=a,b=b,loops=loops,offset=offset*i,spacing=spacing)) 
+    for i in range(n):
+        wheel.o = offset*i
+        sd.add(spiro(R=R,wheel=wheel,loops=loops,spacing=spacing)) 
     return sd
 
 # def roll_orig(x1,y1,x2,y2,a,b,offset=0,guard=0,invert=False):
@@ -153,13 +166,18 @@ def spiro_steps(R=10,a=4,b=3.5,loops=1,n=10,offset=pi/10,spacing=pi/2000):
 #     
 #     return sd
 
-def roll(x1,y1,x2,y2,a,b,offset=0,start_guard=0,end_guard=0,invert=False):
+def roll(x1,y1,x2,y2,wheel,start_guard=0,end_guard=0,invert=False):
     '''roll in straight line from (x1,y1) to (x2,y2)
     using wheel of diameter a and pen position b.
     offset is the start angle off the vertical for the pen, in radians.
     invert keyword controls sense of wheel location:  default
     is above and/or to the right.  invert=True is the opposite.
     '''
+    
+    a = wheel.r
+    b = wheel.m
+    offset = wheel.o
+    
     R=sqrt((x2-x1)**2+(y2-y1)**2) - (start_guard+end_guard)  # roll distance
     A=arctan2(y2-y1,x2-x1)                                   # roll angle
     t=np.linspace(0,R/a,1000)           # angle through which the wheel rolls
@@ -231,7 +249,7 @@ def rot_coords(angle_rads,coords):
 
     return cc
 
-def spiro_line_orig(R=60,a=12,b=7.2,orient=0,loops=60,n=1,fold=False, invert=False):
+def spiro_line_orig(R=60,wheel=Wheel(12,7.2),orient=0,loops=60,n=1,fold=False, invert=False):
 
     cc = rot_coords(orient,array([ [-R/2,0], [R/2,0] ]))
 
@@ -249,58 +267,44 @@ def spiro_line_orig(R=60,a=12,b=7.2,orient=0,loops=60,n=1,fold=False, invert=Fal
         
             cn=(c+1) % cc.shape[0]
             
-            sd.add(roll(cc[c,0],cc[c,1],cc[cn,0],cc[cn,1],a,b,offset,invert=invert))  # cycloids roll
-            offset=sd.pc()
-        
+            sd.add(roll(cc[c,0],cc[c,1],cc[cn,0],cc[cn,1],wheel,invert=invert))  # cycloids roll        
             sd.add(rotate(cc[cn,0],cc[cn,1],sd.xc(),sd.yc(),rot_angle)) # roll over upper right
-            offset+=rot_angle
+            wheel.o += rot_angle
         
     return sd
 
-def spiro_line(R=60,a=12,b=7.2,orient=0,offset=0,loops=60,n=1,fold=False, invert=False):
+def spiro_line(R=60,wheel=Wheel(12,7.2,0),orient=0,loops=60,n=1,fold=False, invert=False):
     coords=array([ [-R/2,0], [R/2,0] ])
-    return spiro_polygon(coords,a,b,orient,
-                         offset=offset,loops=loops,fold=fold,inside=False)  # invert doesn't work
+    return spiro_polygon(coords,wheel,orient,loops=loops,fold=fold,inside=False)  # invert doesn't work
 
-def spiro_eq_triangle(R=60,a=12,b=7.2,orient=0,offset=0,loops=60,n=1,fold=False,inside=False):
+def spiro_eq_triangle(R=60,wheel=Wheel(12,7.2,0),orient=0,loops=60,n=1,fold=False,inside=False):
     ytop = R*sin(pi/3.0)
     coords = array([ [-R/2,-ytop/3], [0,2*ytop/3], [R/2,-ytop/3] ])
-    return spiro_polygon(coords,a,b,orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
+    return spiro_polygon(coords,wheel,orient,loops=loops,fold=fold,inside=inside)
 
-def spiro_square(R=60,a=12,b=7.2,orient=0,offset=0,loops=60,fold=False,inside=False):
+def spiro_square(R=60,wheel=Wheel(12,7.2),orient=0,loops=60,fold=False,inside=False):
     coords = array([ [-R/2,R/2], [R/2,R/2], [R/2,-R/2], [-R/2,-R/2] ])
-    return spiro_polygon(coords,a,b,orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
+    return spiro_polygon(coords,wheel,orient,loops=loops,fold=fold,inside=inside)
 
 def spiro_cross(width=30,height=30,fwidth=1/3,fheight=1/3,base=1/3,
-                wheel=0.01,pen=0,orient=0,offset=0,loops=1,
+                wheel=Wheel(0.01,0),orient=0,loops=1,
                 fold=False,inside=False):
     (x0,x1,x2,x3) = (-width/2, -fwidth*width/2, fwidth*width/2, width/2)
     (y0,y1,y2,y3) = (height*(-base-fheight/2), -height*fheight/2, height*fheight/2, height*(1-fheight/2-base))
     coords = array([ [x0,y2], [x1,y2], [x1,y3], [x2,y3], [x2,y2], [x3,y2],
                      [x3,y1], [x2,y1], [x2,y0], [x1,y0], [x1,y1], [x0,y1] ])
+    return spiro_polygon(coords,wheel,orient=orient,loops=loops,fold=fold,inside=inside)
     
-#    coords = array([ [-width/2,height*fheight/2], [-fwidth*width/2,height*fheight/2],
-#                     [-fwidth*width/2,height/2],  [fwidth*width/2,height/2],
-#                     [fwidth*width/2,height*fheight/2], [width/2,height*fheight/2],
-#                     [width/2,-fheight*height/2], [fwidth*width/2,-fheight*height/2],
-#                     [fwidth*width/2,-height/2], [-fwidth*width/2,-height/2],
-#                     [-fwidth*width/2,-fheight*height/2], [-width/2,-fheight*height/2] ])
-    return spiro_polygon(coords,wheel,pen,orient=orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
-    
-def spiro_ngon(n,R=60,a=12,b=7.2,orient=0,offset=0,loops=1,fold=False,inside=False):
+def spiro_ngon(n,R=60,wheel=Wheel(12,7.2),orient=0,loops=1,fold=False,inside=False):
     coords = np.empty((n,2))
     for i in range(n):
         theta = -2*pi*i/n
         coords[i,0]=R*cos(theta)
         coords[i,1]=R*sin(theta)
-    return spiro_polygon(coords,a,b,orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
+    return spiro_polygon(coords,wheel,orient,loops=loops,fold=fold,inside=inside)
     
-def spiro_nstar(n,r1=30,r2=0.5,wheel=0.01,pen=0,
-                orient=0,offset=0,loops=1,fold=False,inside=False):
+def spiro_nstar(n,r1=30,r2=0.5,wheel=Wheel(0.01,0),
+                orient=0,loops=1,fold=False,inside=False):
     coords = np.empty((2*n,2))
     for i in range(0,n):
         theta1 = -2*pi*i/n
@@ -310,8 +314,9 @@ def spiro_nstar(n,r1=30,r2=0.5,wheel=0.01,pen=0,
         coords[2*i+1,0]=r2*r1*cos(theta2)
         coords[2*i+1,1]=r2*r1*sin(theta2)
         
-    return spiro_polygon(coords,wheel,pen,orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
+    return spiro_polygon(coords,wheel,orient,
+                         loops=loops,fold=fold,inside=inside)
+
 def corner_angles(coords,inside=False):
     '''find the angle between adjacent pairs of coords'''
     ba = np.empty(coords.shape[0])
@@ -332,7 +337,7 @@ def corner_angles(coords,inside=False):
 
     return ba
             
-def spiro_polygon(coords,wheel,pen,orient=0,loops=1,offset=0,fold=False,inside=False):
+def spiro_polygon(coords,wheel,orient=0,loops=1,fold=False,inside=False):
 
     cc = rot_coords(orient,coords)
     ba = corner_angles(cc,inside)
@@ -341,7 +346,7 @@ def spiro_polygon(coords,wheel,pen,orient=0,loops=1,offset=0,fold=False,inside=F
 
     for c in range(ba.shape[0]):
         if ba[c]>0:
-            bump[c] = corner_guard(wheel,ba[c])
+            bump[c] = corner_guard(wheel.r,ba[c])
             ba[c]=0  # to indicate no rotation
         else:
             ba[c]*=-1 # rotation angles
@@ -355,9 +360,9 @@ def spiro_polygon(coords,wheel,pen,orient=0,loops=1,offset=0,fold=False,inside=F
             cp=(c+cc.shape[0]-1) % cc.shape[0]
             cn=(c+1) % cc.shape[0]
 
-            sd.add(roll(cc[c,0],cc[c,1],cc[cn,0],cc[cn,1],wheel,pen,offset,
+            sd.add(roll(cc[c,0],cc[c,1],cc[cn,0],cc[cn,1],wheel,
                         start_guard=bump[cp],end_guard=bump[c],invert=inside)) 
-            offset=sd.pc()
+            wheel.o=sd.pc()
 
             if ba[c]>0:
                 if inside: rot_angle= -ba[c]
@@ -365,75 +370,71 @@ def spiro_polygon(coords,wheel,pen,orient=0,loops=1,offset=0,fold=False,inside=F
                 if fold:
                     rot_angle=ba[c]+2*pi if inside else ba[c]-2*pi
                 sd.add(rotate(cc[cn,0],cc[cn,1],sd.xc(),sd.yc(),rot_angle)) # roll over upper right
-                offset+=rot_angle
+                wheel.o+=rot_angle
     
     return sd
 
-def poly_heart(width=40,depth=20,height=5,wheel=2.2,pen=1.0,orient=0,offset=0,loops=1,
+def poly_heart(width=40,depth=20,height=5,wheel=Wheel(2.2,1.0),orient=0,loops=1,
                fold=False,inside=False,guarded=True):
     coords = array([ [0,-depth], [-width/2,-height/2], [-width/2+width/12,0], [-width/6,height],
                      [0,0], [width/6,height], [width/2-width/12,0], [width/2,-height/2] ])
-    return spiro_polygon(coords,wheel,pen,orient,
-                         offset=offset,loops=loops,fold=fold,inside=inside)
+    return spiro_polygon(coords,wheel,orient,loops=loops,fold=fold,inside=inside)
 
-def heart(x0=0,y0=0,width=40,depth=20,wheel=2.2,pen=1.0,loops=1,offset=0.0,
+def heart(x0=0,y0=0,width=40,depth=20,wheel=Wheel(2.2,1.0),loops=1,
           inside=False,fold=False,guarded=True):
 
     sd = SpiroData()
 
     g=1 if guarded else 0
-    o=offset
     
     for k in range(loops):
 
         if inside:
 
             ba=corner_angles(np.array([ [x0,y0-depth], [x0-width/2,y0], [x0-width/2,y0+1] ]),inside=True)
-            cg=corner_guard(wheel,ba[0])
+            cg=corner_guard(wheel.r,ba[0])
             
-            sd.add(roll(x0,y0-depth,x0-width/2,y0,wheel,pen,offset=o,
-                        start_guard=g*corner_guard(wheel,pi/2),end_guard=g*cg,
-                        invert=True))
-            sd.add(spiro_arc(x0-width/4,y0,-pi/2,width/4,wheel,pen,loops=0.5,offset=sd.pc(),
-                             start_guard=g*cg,
-                             invert=True))
+            sd.add(roll(x0,y0-depth,x0-width/2,y0,wheel,
+                        start_guard=g*corner_guard(wheel.r,pi/2),end_guard=g*cg,invert=True))
+            wheel.o=sd.pc()
+            sd.add(spiro_arc(x0-width/4,y0,-pi/2,width/4,wheel,loops=0.5,start_guard=g*cg,invert=True))
             
-            o = sd.pc()
+            wheel.o = sd.pc()
             rot_angle = pi if fold else -pi
             sd.add(rotate(x0,y0,sd.xc(),sd.yc(),rot_angle))
-            sd.add(spiro_arc(x0+width/4,y0,-pi/2,width/4,wheel,pen,loops=0.5,offset=o+rot_angle,
-                             end_guard=g*cg,
-                             invert=True))
-            sd.add(roll(x0+width/2,y0,x0,y0-depth,wheel,pen,offset=sd.pc(),
-                        start_guard=g*corner_guard(wheel,pi-pi/4),
-                        end_guard=g*corner_guard(wheel,pi/2),
+            wheel.o += rot_angle
+            sd.add(spiro_arc(x0+width/4,y0,-pi/2,width/4,wheel,loops=0.5,end_guard=g*cg,invert=True))
+            wheel.o = sd.pc()
+            sd.add(roll(x0+width/2,y0,x0,y0-depth,wheel,
+                        start_guard=g*corner_guard(wheel.r,pi-pi/4),
+                        end_guard=g*corner_guard(wheel.r,pi/2),
                         invert=True))
-            o=sd.pc()
+            wheel.o=sd.pc()
 
         else:
             
-            sd.add(roll(x0,y0-depth,x0-width/2,y0,wheel,pen,offset=o))
-            o = sd.pc()
+            sd.add(roll(x0,y0-depth,x0-width/2,y0,wheel))
+            wheel.o = sd.pc()
             rot_angle = pi/4 - 2*pi if fold else pi/4
             sd.add(rotate(x0-width/2,y0,sd.xc(),sd.yc(),rot_angle))
 
-            ca=cos_angle(width/4+wheel,width/4+width/4,width/4+wheel)
+            ca=cos_angle(width/4+wheel.r,width/4+width/4,width/4+wheel.r)
+            wheel.o += rot_angle
+            
+            sd.add(spiro_arc(x0-width/4,y0,-pi/2,width/4,wheel,loops=0.5,end_guard_angle=g*ca))
 
-            sd.add(spiro_arc(x0-width/4,y0,-pi/2,width/4,wheel,pen,loops=0.5,offset=o+rot_angle,
-                             end_guard_angle=g*ca))
-
-            ca=cos_angle(width/4+width/4,width/4+wheel,width/4+wheel)
-
-            sd.add(spiro_arc(x0+width/4,y0,-pi/2,width/4,wheel,pen,loops=0.5,offset=sd.pc(),
-                             start_guard_angle=g*ca))
-            o = sd.pc()
+            ca=cos_angle(width/4+width/4,width/4+wheel.r,width/4+wheel.r)
+            wheel.o = sd.pc()
+            sd.add(spiro_arc(x0+width/4,y0,-pi/2,width/4,wheel,loops=0.5,start_guard_angle=g*ca))
+            wheel.o = sd.pc()
             rot_angle = pi/4 - 2*pi if fold else pi/4
             sd.add(rotate(x0+width/2,y0,sd.xc(),sd.yc(),rot_angle))
-            sd.add(roll(x0+width/2,y0,x0,y0-depth,wheel,pen,offset=o+rot_angle))
-            o = sd.pc()
+            wheel.o += rot_angle
+            sd.add(roll(x0+width/2,y0,x0,y0-depth,wheel))
+            wheel.o = sd.pc()
             rot_angle = pi/2 - 2*pi if fold else pi/2
             sd.add(rotate(x0,y0-depth,sd.xc(),sd.yc(),rot_angle))
-            o += rot_angle
+            wheel.o += rot_angle
 
     return sd
 
