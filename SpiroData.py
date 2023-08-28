fixed wheels in ellipses algorithm, implemented rotation on SpiroData for ellipse orientation

SpiroData.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy import array, append, column_stack
+from SpiroGeometry import *
 
 class SpiroData:
     
@@ -6,32 +8,36 @@ class SpiroData:
         self.reset()
         
     def reset(self):
-        self.x = np.array([])
-        self.y = np.array([])
-        self.p = np.array([])
-        self.t = np.array([])   # parameterize the drawing data (not sure if this is needed yet)
+        self.x = array([])
+        self.y = array([])
+        self.p = array([])
+        self.t = array([])   # parameterize the drawing data (not sure if this is needed yet)
 
     def add(self, sd):
-        self.x = np.append(self.x,sd.x)
-        self.y = np.append(self.y,sd.y)
-        self.p = np.append(self.p,sd.p)
-        self.t = np.append(self.t,sd.t)
+        self.x = append(self.x,sd.x)
+        self.y = append(self.y,sd.y)
+        self.p = append(self.p,sd.p)
+        self.t = append(self.t,sd.t)
 
     def set(self,x=0,y=0,p=0,t=0):
-        self.x = np.array([x])
-        self.y = np.array([y])
-        self.p = np.array([p])
-        self.t = np.array([t])
+        self.x = array([x])
+        self.y = array([y])
+        self.p = array([p])
+        self.t = array([t])
 
     def set_array(self,x,y,p,t):
-        for i in range(x.shape[0]):
-            np.append(self.x,x)
-            np.append(self.y,y)
-            np.append(self.p,y)
-            np.append(self.t,t)
+        self.x = x
+        self.y = y
+        self.p = p
+        self.t = t
     
     def xc(self):  return self.x[-1] 
     def yc(self):  return self.y[-1] 
     def pc(self):  return self.p[-1]
     def tc(self):  return self.t[-1]
     
+    def rotate(self,angle):
+        coords = rot_coords(angle,column_stack((self.x,self.y)))
+        self.x = coords[:,0]
+        self.y = coords[:,1]
+        

diagrams.py
@@ -74,7 +74,7 @@ def ring_wheel_diagram(ring=Ring(radius=20,origin=np.array([0,0]),orient=0),whee
         
     return sd
 
-def ring_ellipse_diagram(ring=Ellipse(20,0.5),wheel=Wheel(4,3,0.0),phi0=0,inside=True):
+def ring_ellipse_diagram(ring=Ellipse(20,0.5),wheel=Wheel(4,3,0.0),phi0=0,inside=True,orient=0):
 
     # Draw the ring
     
@@ -89,7 +89,7 @@ def ring_ellipse_diagram(ring=Ellipse(20,0.5),wheel=Wheel(4,3,0.0),phi0=0,inside
 
     iv = -1 if inside else 1
 
-    phi = wheel.o
+    phi = wheel.o - orient
 
     theta = iv * ring.phi_at_arc(wheel.arc(phi-phi0)) + theta_offset
 
@@ -122,4 +122,6 @@ def ring_ellipse_diagram(ring=Ellipse(20,0.5),wheel=Wheel(4,3,0.0),phi0=0,inside
 
     sd.add(line(np.array([ [x0_wheel,y0_wheel],[x0_pen,y0_pen] ])))
 
+    sd.rotate(orient)
+    
     return sd

spiro.py
@@ -15,7 +15,7 @@ def wheel_in_ellipse(x0=0,y0=0,wheel=Wheel(4,3.5,0),ellipse=Ellipse(10,0.5,0,0),
               loops=1,pts_per_loop=4000,
               slide = lambda t: 1,
               start_guard=0,end_guard=0,start_guard_angle=0,end_guard_angle=0,
-              invert=False,reverse=False):  
+              orient=0,invert=False,reverse=False):  
     '''roll on the outside (inside if invert=True) of an elliptical arc
     centered on x0, y0.  Direction of motion can be reversed by setting reverse=True
     '''
@@ -26,13 +26,12 @@ def wheel_in_ellipse(x0=0,y0=0,wheel=Wheel(4,3.5,0),ellipse=Ellipse(10,0.5,0,0),
 
     a = wheel.r
     m = wheel.m
-    offset = wheel.o
     
     t=np.linspace(0,loops,int(loops*pts_per_loop))
     
     if reverse: t *= -1
     
-    p = t * ellipse.c / wheel.c * 2 * pi + wheel.o
+    p = t * ellipse.c / wheel.c * 2 * pi + wheel.o - orient
 
     theta = np.array([ iv * ellipse.phi_at_arc(wheel.arc(phi)) + ellipse.o for phi in p ])
 
@@ -48,6 +47,8 @@ def wheel_in_ellipse(x0=0,y0=0,wheel=Wheel(4,3.5,0),ellipse=Ellipse(10,0.5,0,0),
     sd.x=x0_wheel + m*sin(p)
     sd.y=y0_wheel + m*cos(p)
     sd.p=p
+
+    sd.rotate(orient)
     
     return sd
 